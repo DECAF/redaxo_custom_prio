8067e1be6a28a87469de3388ab8af59469aca781
Dirk Schuerjohann <ds@decaf.de>
2010-12-22 13:22:46 +0000

    add comments to config

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,13 +1,10 @@
 ; <?php die('No access');
 
-; Root category has ID 0
+; root category (ID 0)
+; -> keep default prio for categories (~ blank value)
+; -> set default prio for articles to 2
+; -> inherit the values
 [0]
-new_category_prio   = 100
+new_category_prio   =
 new_article_prio    = 2
 inherit             = true
-
-; ; Example for category with ID 2
-; [2]
-; new_category_prio   = 1
-; new_article_prio    = 2
-; inherit             = true
